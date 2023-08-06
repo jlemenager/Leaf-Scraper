23221c1e3cd1157740849c28826c2490ec74160c
jlemenager <jeremy@unitedbyzero.com>
2023-08-06 00:18:54 -0400

    added scraper code

---
+++ scraper.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const PORT = 4000
+const app = express()
+const cors = require('cors')
+const puppeteer = require('puppeteer')
+const fs = require('fs/promises')
+
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+app.use(cors())
+
+let names;
+let h2s;
+const FindH1 = async(url) =>{
+    // const filepath = path.join(__dirname, `${name}.txt`)
+    const browser = await puppeteer.launch({ headless: false })
+    const page = await browser.newPage()
+    console.log('scraper running')
+
+    await page.goto(url)
+    names = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll("h1")).map(x => x.textContent)
+    })
+
+    console.log(names)
+
+    // await page.screenshot({ path: `leaf_project/src/images/website.png`})
+    await browser.close()
+    return names
+
+}
+
+
+app.post('/getinfo', async(req,res) =>{
+    console.log('logging body', req.body)
+    let response = await FindH1(req.body.url)
+    console.log(response)
+    res.send(response)
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`)
+})
+
+//thanks to chatgpt and puppeteers documentation, youtube videos etc for this
